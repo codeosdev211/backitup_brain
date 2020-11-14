4e1097a22424a36f25bdad6bab0076ed491d2e0b
codeos <codeos.dev211@gmail.com>
2020-11-14 17:26:26 +0530

    commit 14Nov20 1726

--- src/routes/groups.go
+++ src/routes/groups.go
@@ -30,7 +30,6 @@ func GetGroups(res http.ResponseWriter, req *http.Request) {
         helper.SendErrorResponse(&res, "Invalid Request Body")
     }
 
-    /* getting request body */
     query := fmt.Sprintf("select BG.code, BG.name, BG.userCount, BU.firstName from BG left join BU on BG.ownerCode like '%%';")
     response.Data, err = db.CallDatabase(true, &query)
     if err != nil {
