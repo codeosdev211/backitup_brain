b0cf966ba8a67abc5d40143772d0eeb6caf15883
codeos <codeos.dev211@gmail.com>
2020-11-18 17:50:36 +0530

    commit 18Nov20 1750

--- src/routes/groups.go
+++ src/routes/groups.go
@@ -11,6 +11,27 @@ import (
     helper "../commons"
 )
 
+func MembersList(res http.ResponseWriter, req *http.Request) {
+    /* checking for request type */
+    if req.Method != "POST" {
+        helper.SendErrorResponse(&res, "Invalid Request Type")
+    }
+
+    var request models.Request
+    var response models.Response
+    response.Status = 0
+    response.Msg = "none"
+    response.Data =  nil
+
+    /*validating request json object */
+    err := json.NewDecoder(req.Body).Decode(&request)
+    if err != nil {
+        helper.SendErrorResponse(&res, "Invalid Request Body")
+    }
+    //body := request.Values[0]
+
+}
+
 
 func GetGroups(res http.ResponseWriter, req *http.Request) {
     /* checking for request type */
@@ -33,7 +54,7 @@ func GetGroups(res http.ResponseWriter, req *http.Request) {
 
     body := request.Values[0]
 
-    query := fmt.Sprintf("select BG.code, BG.name, BG.ownerCode, BG.userCount, BU.firstName from BG left join BU on BG.ownerCode like '%%';")
+    query := fmt.Sprintf("select BG.code, BG.name, BG.ownerCode, BG.userCount, BU.firstName from BG left join BU on BG.code like '%%';")
     response.PublicGroups, err = db.CallDatabase(true, &query)
     if err != nil {
         helper.SendErrorResponse(&res, "Database error")
@@ -43,8 +64,8 @@ func GetGroups(res http.ResponseWriter, req *http.Request) {
      joining THREE tables should do, right? ... YES!
     */
 
-    query = fmt.Sprintf("select BG.code, BG.name, BG.userCount, BU.firstName from BUG right join BG on BUG.groupCode = BG.code "+
-                        " right join BU on BG.ownerCode = BU.code where BUG.userCode='%v' ;",
+    query = fmt.Sprintf("select BG.code, BG.name, BG.userCount, BU.firstName from BUG right join BG "+
+                        " right join BU on BG.ownerCode = BU.code on BUG.groupCode = BG.code where BUG.userCode='%v' ;",
                         body["code"])
     response.UserGroups, err = db.CallDatabase(true, &query)
     if err != nil {
